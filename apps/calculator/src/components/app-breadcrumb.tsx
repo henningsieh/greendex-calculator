89ccdc2dd215ecd7a0bda1860202f703919059d2
Henning Sieh <admin@henningsieh.de>
2026-09-07 16:20:23 +0200

    chore(oxc): share strict application lint policy

--- apps/calculator/src/components/app-breadcrumb.tsx
+++ apps/calculator/src/components/app-breadcrumb.tsx
@@ -12,10 +12,9 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { Skeleton } from "@/components/ui/skeleton";
+import { ORGANIZATION_ICONS } from "@/features/organizations/organization-icons";
 import { PROJECT_ICONS } from "@/features/projects/components/project-icons";
 import { Link, usePathname } from "@/lib/i18n/routing";
-
-import { ORGANIZATION_ICONS } from "../features/organizations/organization-icons";
 
 /**
  * AppBreadcrumb
